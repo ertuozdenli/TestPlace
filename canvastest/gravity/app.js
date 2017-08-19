6bcc60aecf07b07fdb876f8f9b16a2be55b7f8c1
ertuozdenli <ertu.ozdenli@gmail.com>
2017-08-19 23:18:13 +0300

    Multi

--- canvastest/gravity/app.js
+++ canvastest/gravity/app.js
@@ -27,36 +27,53 @@ class yuvarlak {
 
 var ortax = genislik / 2;
 var ortah = yukseklik / 2;
-var hiz = 5;
-var yercekimi = 0.85;
+var yercekimi = 0.90;
 // var randx = Math.random() * genislik;
 // var randy = Math.random() * yukseklik;
 var kullanicix = ortax,
     kullaniciy = ortah;
 
+    var konum = [[ortax,ortah]];
+    var hiz = [5];
+    var adet = 1;
 window.addEventListener("click", function (e) {
     kullanicix = e.clientX;
     kullaniciy = e.clientY;
-    // nesne.yciz(kullanicix, kullaniciy, 50, 50);
+    
+    
+    nesne.yciz(kullanicix, kullaniciy, 50, 50);
+    konum[adet] = [kullanicix, kullaniciy];
+    adet++;
+    hiz.push(5);
 });
 
 var nesne = new yuvarlak();
+
+
 
 function animate() {
     requestAnimationFrame(animate);
 
     c.clearRect(0, 0, genislik, yukseklik);
 
-    nesne.yciz(kullanicix, kullaniciy, 50, 50);
-
-
-    if (kullaniciy + 50 > yukseklik) {
-        hiz = -hiz * yercekimi;
-    } else {
-        hiz += 1;
+    // nesne.yciz(kullanicix, kullaniciy, 50, 50);
+    for (var i = 0; i < adet; i++) {
+        
+        nesne.yciz(konum[i][0], konum[i][1], 50, 50);
+        
+        if (konum[i][1] + 50 > yukseklik) {
+            hiz[i] = -hiz[i] * yercekimi;
+        } else {
+            hiz[i] += 1;
+        }
+    
+        konum[i][1] += hiz[i];
     }
 
-    kullaniciy += hiz;
+    
+   
+
+    
 
 }
 
